26177aa34835d2702867bc62be6e6246c6f1734e
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2018-06-15 01:26:58 -0300

    Measuring performance of removing '.' and '..'
    From reading the directory content, checking different algorithms to
    remove the '.' and '..' and measuring the performance of each one.

--- performance/perf2.pl
+++ performance/perf2.pl
@@ -16,13 +16,117 @@ for ( 1 .. 1000 ) {
     close($fh);
 }
 
+my $regex = qr/^\.\.?$/;
+
 $bench->add_instances(
     Dumbbench::Instance::PerlSub->new(
-        name => 'skipping with grep',
+        name => 'with grep',
         code => sub {
             opendir my $dir_h, $dir or die "Cannot open $dir: $!";
             my @top_entries = grep { $_ !~ /^\.\.?$/ } readdir $dir_h;
             closedir($dir_h);
+        }
+    ),
+    Dumbbench::Instance::PerlSub->new(
+        name => 'with grep and compiled regex',
+        code => sub {
+            opendir my $dir_h, $dir or die "Cannot open $dir: $!";
+            my @top_entries = grep { $_ !~ $regex } readdir $dir_h;
+            closedir($dir_h);
+        }
+    ),
+    Dumbbench::Instance::PerlSub->new(
+        name => 'with eq',
+        code => sub {
+            opendir my $dir_h, $dir or die "Cannot open $dir: $!";
+            my @temp = readdir($dir_h);
+            closedir($dir_h);
+            my @top_entries;
+            for (@temp) {
+                next if ( ( $_ eq '.' ) or ( $_ eq '..' ) );
+                push( @top_entries, $_ );
+            }
+        }
+    ),
+    Dumbbench::Instance::PerlSub->new(
+        name => 'with enhanced eq',
+        code => sub {
+            opendir my $dir_h, $dir or die "Cannot open $dir: $!";
+            my @temp = readdir($dir_h);
+            closedir($dir_h);
+            my @top_entries;
+            for (@temp) {
+                next
+                  if (  ( length($_) <= 2 )
+                    and ( ( $_ eq '.' ) or ( $_ eq '..' ) ) );
+                push( @top_entries, $_ );
+            }
+        }
+    ),
+    Dumbbench::Instance::PerlSub->new(
+        name => 'with enhanced eq mark2',
+        code => sub {
+            opendir my $dir_h, $dir or die "Cannot open $dir: $!";
+            my @temp = readdir($dir_h);
+            closedir($dir_h);
+            my @top_entries;
+            my $counter = 0;
+            my $found   = 0;
+
+            for (@temp) {
+                $counter++;
+
+                if (    ( length($_) <= 2 )
+                    and ( ( $_ eq '.' ) or ( $_ eq '..' ) ) )
+                {
+                    $found++;
+                    if ( $found < 2 ) {
+                        next;
+                    }
+                    else {
+                        push( @top_entries, splice( @temp, $counter ) );
+                        last;
+                    }
+
+                }
+                push( @top_entries, $_ );
+            }
+        }
+    ),
+    Dumbbench::Instance::PerlSub->new(
+        name => 'with enhanced eq mark3',
+        code => sub {
+            opendir my $dir_h, $dir or die "Cannot open $dir: $!";
+            my @temp = readdir($dir_h);
+            closedir($dir_h);
+            my ( $first, $second );
+            my $index = 0;
+            my $found = 0;
+
+            for (@temp) {
+
+                if (    ( length($_) <= 2 )
+                    and ( ( $_ eq '.' ) or ( $_ eq '..' ) ) )
+                {
+                    if ( $found < 1 ) {
+                        $first = $index;
+                        $found++;
+                        $index++;
+                        next;
+                    }
+                    else {
+                        $second = $index;
+                        last;
+                    }
+
+                }
+                else {
+                    $index++;
+                }
+            }
+
+            splice( @temp, $first,  1, 1 );
+            splice( @temp, $second, 1, 1 );
         }
     ),
     Dumbbench::Instance::PerlSub->new(
